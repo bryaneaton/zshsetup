7f4725f45dec73eb6638dcadb8b0e297da779e9a
Bryan Eaton <bryan.eaton77@gmail.com>
2018-10-16 11:43:50 -0400

    First commit

---
+++ setupZSHEnv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt-get update
+apt-get install zsh fonts-powerline git -y
+chsh -s $(which zsh)
+
+cd ~/Downloads
+
+# Clone agnoster
+git clone https://github.com/agnoster/agnoster-zsh-theme.git
+
+
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+#Copy agnoster to theme directory
+
+cp ~/Downloads/agnoster-zsh-theme/agnoster.zsh-theme ~/.oh-my-zsh/custom/themes/agnoster.zsh-theme
