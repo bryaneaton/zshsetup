257508346cad35e517836795009dcc2a60a38aa8
Bryan Eaton <bryan.eaton77@gmail.com>
2018-10-16 12:29:39 -0400

    removed a line

--- setupZSHEnv.sh
+++ setupZSHEnv.sh
@@ -8,14 +8,13 @@ cd ~/Downloads
 # Clone agnoster
 git clone https://github.com/agnoster/agnoster-zsh-theme.git
 
-
 # Remove Oh My Zsh
 rm -r /home/bryan/.oh-my-zsh
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 #Copy agnoster to theme directory
-
+git 
 cp ~/Downloads/agnoster-zsh-theme/agnoster.zsh-theme ~/.oh-my-zsh/custom/themes/agnoster.zsh-theme
 
 
